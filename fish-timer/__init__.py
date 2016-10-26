8c06da5b718d98980f5e181253c68db3567752d4
Jayd Lawrence <jayd.lawrence@gmail.com>
2016-10-26 20:37:51 +0100

    initial commit

---
+++ fish-timer/__init__.py
@@ -0,0 +1,5 @@
+from __future__ import absolute_import
+from .fish-timer import *
+
+
+__version__ = '1.0.1'
---
+++ fish-timer/fish-timer.py
@@ -0,0 +1,22 @@
+import sched, time
+from energenie import switch_on, switch_off
+
+ON_DURATION = 5 # * 60
+OFF_DURATION = 10 # * 60
+
+
+
+
+def fish_pump_on ():
+    switch_on()
+    s = sched.scheduler(time.time, time.sleep)
+    s.enter(ON_DURATION, 1, fish_pump_off, ())
+    s.run()
+
+def fish_pump_off ():
+    switch_off()
+    s = sched.scheduler(time.time, time.sleep)
+    s.enter(ON_DURATION, 1, fish_pump_on, ())
+    s.run()
+
+fish_pump_on()
