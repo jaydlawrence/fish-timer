ad7e9efa2db7e6bdb5abb27eb5bdcdeeae298a16
Jayd Lawrence <jayd.lawrence@gmail.com>
2016-10-26 20:54:36 +0100

    a bit of printouts to see what is going on and use the correct duration for the off cycle

--- fish-timer/fish-timer.py
+++ fish-timer/fish-timer.py
@@ -9,14 +9,16 @@ OFF_DURATION = 10 # * 60
 
 def fish_pump_on ():
     switch_on()
+    print "Switching on"
     s = sched.scheduler(time.time, time.sleep)
     s.enter(ON_DURATION, 1, fish_pump_off, ())
     s.run()
 
 def fish_pump_off ():
     switch_off()
+    print "Switching off"
     s = sched.scheduler(time.time, time.sleep)
-    s.enter(ON_DURATION, 1, fish_pump_on, ())
+    s.enter(OFF_DURATION, 1, fish_pump_on, ())
     s.run()
 
 fish_pump_on()
